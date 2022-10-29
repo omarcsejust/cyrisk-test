1822e0f997e6e58ba7e8cd94b46a298d2506f619
Omar <omarcsejust@gmail.com>
2022-10-29 20:56:00 +0600

    added choices field in django model

--- cyrisk/hosts/models.py
+++ cyrisk/hosts/models.py
@@ -3,10 +3,21 @@ from django.core.validators import URLValidator
 
 
 class Host(models.Model):
+
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+    CRITICALITY_CHOICES = [
+        (LOW, "low"),
+        (MEDIUM, "medium"),
+        (HIGH, "high")
+    ]
+
     name = models.CharField(null=False, max_length=50)
     # TODO:: here we can implement a custom validators to validate domain, I have a plan to do it later
     domain = models.CharField(max_length=255, null=False, validators=[URLValidator])
     description = models.TextField()
-    criticality = models.CharField(null=False, max_length=50)
+    criticality = models.CharField(null=False, max_length=50, choices=CRITICALITY_CHOICES)
 
 
